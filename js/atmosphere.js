4a0d606e71f113ab203639e81fe17cd2b41304a0
Wiley Davis <wileydavis@gmail.com>
2026-02-05 13:15:55 -0800

    Fix atmosphere z-index: render above background

--- js/atmosphere.js
+++ js/atmosphere.js
@@ -16,7 +16,7 @@
         width: 100%;
         height: 100%;
         pointer-events: none;
-        z-index: 0;
+        z-index: 1;
         opacity: 0.6;
     `;
     document.body.insertBefore(canvas, document.body.firstChild);
